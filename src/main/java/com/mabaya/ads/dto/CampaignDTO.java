0dabfe786a97249d5e8f727e448f79c15ce1794e
Julian Broudy <Julian@Broudy.io>
2023-12-03 17:58:16 +0200

    DTO should hold ids.

--- src/main/java/com/mabaya/ads/dto/CampaignDTO.java
+++ src/main/java/com/mabaya/ads/dto/CampaignDTO.java
@@ -12,11 +12,10 @@ import java.util.Objects;
  * @author <a href="https://github.com/JulianBroudy">Julian Broudy</a>
  */
 public record CampaignDTO(
-    Long id, String name, Timestamp startDate, BigDecimal bid, Collection<Product> products) {
+    Long id, String name, Timestamp startDate, BigDecimal bid, Collection<Long> productIds) {
   public CampaignDTO {
     Objects.requireNonNull(name, "Name must not be null");
     Objects.requireNonNull(startDate, "Start date must not be null");
     Objects.requireNonNull(bid, "Bid must not be null");
-    Objects.requireNonNull(products, "Products must not be null");
   }
 }
