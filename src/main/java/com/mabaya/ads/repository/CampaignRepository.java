13e17f3741b5262e6c56091dbd01f703f73c228f
Julian Broudy <Julian@Broudy.io>
2023-12-05 23:40:24 +0200

    Added explanation for the JPQL query.

--- src/main/java/com/mabaya/ads/repository/CampaignRepository.java
+++ src/main/java/com/mabaya/ads/repository/CampaignRepository.java
@@ -2,7 +2,7 @@ package com.mabaya.ads.repository;
 
 import com.mabaya.ads.model.Campaign;
 import com.mabaya.ads.model.Category;
-import java.sql.Timestamp;
+import java.time.Instant;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,11 +15,21 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
+
+  // Preferred complexity rather than separate call to database.
+  // Ideally would have a more experienced architect to brainstorm.
+  /**
+   * Finds all active campaigns with the highest bid for a given category. The query joins the
+   * Campaign and Product entities, filtering by the campaign's start date to ensure it's active
+   * (within the last 10 days). It then filters by category (if provided) and orders the results by
+   * the campaign's bid in descending order, fetching the top result. If no category is specified,
+   * it defaults to fetching campaigns regardless of the product category.
+   */
   @Query(
       "SELECT c FROM Campaign c JOIN c.products p WHERE c.startDate <= :now AND c.startDate > :nowMinus10Days AND (:category IS NULL OR p.category = :category) ORDER BY c.bid DESC")
   Page<Campaign> findAllActiveCampaignWithHighestBidByCategory(
-      @Param("now") Timestamp now,
-      @Param("nowMinus10Days") Timestamp nowMinus10Days,
+      @Param("now") Instant now,
+      @Param("nowMinus10Days") Instant nowMinus10Days,
       @Param("category") Category category,
       Pageable pageable);
 }
