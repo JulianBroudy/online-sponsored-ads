9c2de5fdb280d97be8f04e2f7701c38b2477bb57
Julian Broudy <Julian@Broudy.io>
2023-12-05 23:31:14 +0200

    Improved @AdviceController.

--- src/main/java/com/mabaya/ads/controller/GlobalExceptionHandler.java
+++ src/main/java/com/mabaya/ads/controller/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.mabaya.ads.controller;
 
+import java.util.NoSuchElementException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +19,14 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
+  private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
   @ExceptionHandler(MethodArgumentTypeMismatchException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ResponseEntity<String> handleMethodArgumentTypeMismatch(
       MethodArgumentTypeMismatchException ex, WebRequest request) {
     String error = "Invalid value for parameter '" + ex.getName() + "': " + ex.getValue();
+    LOGGER.info(error);
     return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
   }
 
@@ -30,7 +35,21 @@ public class GlobalExceptionHandler {
   public ResponseEntity<String> handleConversionFailed(
       ConversionFailedException ex, WebRequest request) {
     String error = "Failed to convert value: " + ex.getValue() + " for " + ex.getTargetType();
+    LOGGER.info(error);
     return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
   }
 
+  @ExceptionHandler(IllegalArgumentException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+    LOGGER.error("Illegal argument exception: {}", ex.getMessage());
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(NoSuchElementException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException ex) {
+    LOGGER.error("No such element: {}", ex.getMessage());
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+  }
 }
