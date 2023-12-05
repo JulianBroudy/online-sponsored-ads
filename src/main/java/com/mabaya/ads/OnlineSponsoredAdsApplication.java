6bb301b4f185592b38423586584fab2ceef40e01
Julian Broudy <Julian@Broudy.io>
2023-12-06 00:16:06 +0200

    Reformatting.

--- src/main/java/com/mabaya/ads/OnlineSponsoredAdsApplication.java
+++ src/main/java/com/mabaya/ads/OnlineSponsoredAdsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineSponsoredAdsApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(OnlineSponsoredAdsApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(OnlineSponsoredAdsApplication.class, args);
+  }
 }
