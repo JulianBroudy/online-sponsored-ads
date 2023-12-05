823b7c2580ef6520ecb8fc16c49fe4a5378fb20e
Julian Broudy <Julian@Broudy.io>
2023-12-05 23:38:47 +0200

    Suppress irrelevant warnings.

--- src/main/java/com/mabaya/ads/service/mapping/IMapper.java
+++ src/main/java/com/mabaya/ads/service/mapping/IMapper.java
@@ -15,6 +15,7 @@ import java.util.Set;
  * @param <DTO> the type parameter representing the Data Transfer Object
  * @author <a href="https://github.com/JulianBroudy">Julian Broudy</a>
  */
+@SuppressWarnings("unused")
 public interface IMapper<MODEL, DTO> {
 
   /**
